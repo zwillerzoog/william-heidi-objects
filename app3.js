20533ab9253814075c1fb5092c0e01f5025c9dbc
Zwillerzoog <marsh.heidi.c@gmail.com>
2017-06-14 16:17:03 -0400

    arrays in objects

--- app3.js
+++ app3.js
@@ -14,3 +14,27 @@ console.log(loaf.flour);
 console.log(loaf.water);
 console.log(loaf.hydration);
 
+//2. Iterating over object's properties
+
+var name = {
+	foo: "bizz",
+	bar: "bang",
+	fum: "fool",
+	quux: "quisp",
+	spam: "email"
+	
+};
+	
+for (let key in name) {
+  console.log(name[key]); //This just displayed "Object Object"
+}
+
+//3. Arrays in objects
+
+let food = {
+	var meals =
+	["breakfast", "second breakfast", "elevenses", "lunch", "afternoon tea", "dinner", "supper"]
+	return meals[3];
+}
+
+console.log(food); //unexpected token at line 5
